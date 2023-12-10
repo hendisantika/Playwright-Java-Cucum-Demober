2f669c37c04aa9e95cf4839e247cc5446edf16e5
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 07:54:56 +0700

    Add createPlaywrightPageInstance in BasePage🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/pages/BasePage.java
+++ src/test/java/com/hendisantika/pages/BasePage.java
@@ -1,7 +1,9 @@
 package com.hendisantika.pages;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +22,27 @@ public abstract class BasePage {
 
     protected Browser browser;
     protected Page page;
+
+    public Page createPlaywrightPageInstance(String browserTypeAsString) {
+        BrowserType browserType = null;
+        switch (browserTypeAsString) {
+            case "Firefox":
+                browserType = Playwright.create().firefox();
+                break;
+            case "Chromium":
+                browserType = Playwright.create().chromium();
+                break;
+            case "Webkit":
+                browserType = Playwright.create().webkit();
+                break;
+
+        }
+        if (browserType == null) {
+            throw new IllegalArgumentException("Could not launch a browser for type " + browserTypeAsString);
+        }
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(true));
+        page = browser.newPage();
+        return page;
+
+    }
 }
